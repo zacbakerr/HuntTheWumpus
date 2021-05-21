eec91f338306b9eca4c2e881363e3648b846f64f
Zac Baker <82305525+zacbakerr@users.noreply.github.com>
2021-05-21 11:07:20 -0400

    Delete Textfield.java

--- Textfield.java
+++
@@ -1,8 +0,0 @@
-
-public class Textfield extends Map { // class to display Text when near Wumpi/Pit/Bats
-  public Textfield() {
-    // Has access to all the variables in MoveShoot(including coords)
-
-  }
-
-}
